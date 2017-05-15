199a2cf8e0584b8e75817b03114e364d1ddcbf91
Nick <numbnick2@gmail.com>
2017-05-14 21:55:16 -0400

    added file structure

---
+++ routes/api-routes.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Nick on 5/14/2017.
+ */
---
+++ server.js
@@ -0,0 +1,42 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+// Requiring Article models
+var Article = require("./models/articles.js");
+
+// Scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+// Initialize Express
+var app = express();
+
+// Use body parser with our app
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://localhost/mongoosearticles");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+    console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+});
+
+//API Routes
+require("./routes/api-routes.js");
